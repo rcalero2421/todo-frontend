44f5d16e4f4c5e9f24819b56235c52b4d231a392
Roberto Calero <rmorck03@gmail.com>
2025-08-03 22:25:34 -0600

    chore: add environment configuration files

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  env: 'stg',
+  baseApiUrl: 'http://staging-api.example.com',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  env: 'prod',
+  baseApiUrl: 'https://api.example.com',
+};
