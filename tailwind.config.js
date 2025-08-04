c7d7a329ed9c9b70334e840594da3348323216ed
Roberto Calero <rmorck03@gmail.com>
2025-08-03 22:25:15 -0600

    chore: update Angular, Tailwind, and project settings

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00A8A3',
+      },
+    },
+  },
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  plugins: [],
+};
